9cf8e1572eef7dd7dc3d7ab551899eaaaa31b5f0
Bilal Cagiran <bilalcagiran@Bilals-MacBook-Pro.local>
2017-09-02 18:42:32 +0100

    initial commit

---
+++ src/App/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  state = {
+    test: true,
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Welcome to React</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
